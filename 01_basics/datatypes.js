dc403fdf79d7da61e7734c0efb7d7039c6713471
hasnainwebdev1 <hasnainmushtaq20hm@gmail.com>
2025-02-12 11:15:08 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"
+
+// alert(3+3)  we are using node.js not browser
+
+console.log(3+3);
+
+console.log("Hasnain");
+
+// datatypes => 2 to power 53
+//number 
+// bigint 
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => 
+// symbol => unique
+// 
+// object
+
+console.log(typeof "Hasnain");
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
+
+
