6f6932ac7210c6240abded5ae93021b1c6bfdeae
hasnainwebdev1 <hasnainmushtaq20hm@gmail.com>
2025-02-12 12:06:47 +0000

    conversion of datatype

---
+++ 01_basics/converstionoperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueinNumber = Number(score)
+
+// console.log(valueinNumber);
+
+// "33" => 33
+// 33abc => nan
+// true => 1; false => 0;
+// 
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Hasnain" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,19 @@
-console.log("Hasnain malik")
+const accountId = 123123
+let usergmail = "hk3809815@gmail.com"
+var accountpassword = "Hello Hasnain"
+usercity = "Layyah"
+let userstate;
+
+usergmail = "Hello hasnain"
+accountpassword = "User Password"
+
+
+
+
+
+
+console.table([accountId, usergmail, accountpassword, usercity, userstate]);
+
+
+
+
