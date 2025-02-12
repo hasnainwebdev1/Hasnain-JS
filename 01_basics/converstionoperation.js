7c859515ef3ccaedf987f2986849e8ea9e11def3
hasnainwebdev1 <hasnainmushtaq20hm@gmail.com>
2025-02-12 18:37:07 +0000

    comparison, converstionoperation, datatypesummary

--- 01_basics/converstionoperation.js
+++ 01_basics/converstionoperation.js
@@ -25,6 +25,56 @@ let booleanisloggedin = Boolean(isloggedin)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***********Operations************
+
+
+let value = 3
+
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(6/2);
+
+let str1 = "Hello"
+let str2 = " Hasnain"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(1+2);
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+
+// console.log(2 + 2 + "1" );
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+  num1 = num2 = num3 = 2+2
+
+  let gamecounter = 100
+   gamecounter++
+   console.log(gamecounter);
+
+
+   let gamecounter1 = 100
+   ++gamecounter1
+   console.log(gamecounter1);
+
+
+
+
 
---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,3 @@
+// Primitive
+
+// 7 Types (Number, String, Boolean, Null, Undefined, Symbol, Bigint)
